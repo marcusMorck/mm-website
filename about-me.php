f8270d83891c379b0b7b6521cc362bff82f1012e
marcusMorck <marcus.morck@gmail.com>
2018-08-22 11:59:11 +0200

    added image to the logo

---
+++ about-me.php
@@ -0,0 +1,49 @@
+<?php
+ include "header.php";
+?>
+<main>
+        <img src="images/profil-image.jpeg" />
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Fusce imperdiet mi sed quam faucibus, quis congue urna viverra. 
+        Vestibulum eu quam cursus, egestas orci sed, fermentum risus.
+         Ut quis ex vel lectus laoreet tincidunt vitae a purus. 
+         Donec eu lacus porttitor, maximus diam vel, placerat erat.
+          Nullam aliquet viverra ligula. Etiam sed pellentesque mauris,
+           eget gravida sem. Donec nec semper odio. Curabitur commodo,
+            enim sed pellentesque laoreet, neque nulla consectetur velit, 
+            id vehicula lacus sapien vitae ex. Sed convallis, 
+            ipsum in placerat finibus, quam erat efficitur libero, vel
+            ultrices ex lorem ac metus.
+Nullam ante erat, gravida id tortor ut, scelerisque imperdiet leo.
+ Fusce lobortis libero id dolor molestie, sit amet vehicula metus 
+ pharetra. Sed volutpat quam nulla, a vestibulum ex commodo a. 
+ Quisque pretium felis at finibus ultrices. Nulla feugiat, ante vel 
+ lacinia iaculis, nulla turpis porta metus, eu dapibus ligula velit 
+ et lorem. Suspendisse ut felis in purus volutpat maximus a sed leo.
+  Maecenas cursus ligula vel lectus congue, vitae ullamcorper elit
+   tempus. Nam efficitur ullamcorper tincidunt.
+Etiam ex neque, molestie ut rutrum eu, placerat non turpis. 
+Sed viverra ultrices ipsum eget pulvinar. Morbi erat dolor, 
+rutrum luctus fringilla vitae, facilisis et lectus. Proin at
+ purus eget dui maximus eleifend. Phasellus lacinia leo at odio 
+ volutpat pharetra. Suspendisse sed nunc a leo posuere lacinia a
+  ut nibh. Nulla aliquam feugiat leo nec mattis. Morbi accumsan 
+  turpis metus, sed faucibus nulla fermentum ut.
+
+Ut laoreet tellus ac risus ullamcorper, quis interdum augue vehicula.
+ Morbi scelerisque porta neque commodo pharetra. In id nibh metus.
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+   Fusce pulvinar elementum nibh, ut luctus nibh consectetur eu.
+    Donec quis tempor dolor. Aenean ac gravida magna. Nunc sit amet
+    us justo, id ultricies nisi. Sed porttitor est id consectetur 
+    auctor. Aenean a eleifend leo. Curabitur congue, enim eget 
+    luctus sollicitudin, nisl nisl vehicula purus, id venenatis 
+    tortor tortor ac odio. In tincidunt justo a orci ultrices 
+    ultricies. Curabitur cursus turpis ac molestie varius. 
+    Nulla sit amet dolor eget augue condimentum sagittis vitae et 
+    lectus. Proin volutpat at purus quis dictum. Mauris in risus 
+    nec purus scelerisque imperdiet fermentum mollis justo.</p>
+</main>
+<?php
+ include "footer.php";
+?>
--- header.php
+++ header.php
@@ -5,11 +5,15 @@
     </head>
     <body>
         <header>
+        <a href="index.php">
             <div class="logo">
-                <a href="#"><h1>Marcus Morck</h1></a>
+                    <img src="images/profil-image.jpeg" />
+               <h1>Marcus Morck</h1>
+               
             </div>
+            </a>
             <nav class="main-nav">
-                <a href="#">About me</a>
+                <a href="about-me.php">About me</a>
                 <a href="#">Portfolio</a> 
                 <a href="#">CV</a>
                 <a href="#">Contact</a> 
