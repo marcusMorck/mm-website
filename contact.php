730f29e7b6c3bfb617119eb29522292635eda666
marcusMorck <marcus.morck@gmail.com>
2018-09-11 10:13:57 +0200

    added hero image

--- contact.php
+++ contact.php
@@ -1,7 +1,31 @@
 <?php
  include "header.php";
 ?>
+<main role="main">
+    <div class="hero">
+    <img src="images/hero-contact.jpg">
+    </div>
+    <h2>Contact me</h2>
 
+    <p>
+    Feel free to contact me! Send an email or use this form to get in contact. I will reply
+    back as soon as possible, or if you need to get in touch with me right away you can always use
+    the phone.
+    </p>
+    <form action="">
+        <label for="forname">* Name:</label>
+        <input type="text" name="forname">
+
+        <label for="lastname">* lastname:</label>
+        <input type="text" name="lastname">
+
+
+        <label for="email">* lastname:</label>
+        <input type="email" name="email">
+
+        <textarea></textarea>
+    </form>
+</main>
 <?php
  include "footer.php";
 ?>
