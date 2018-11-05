0e62ce5faf859ee847f01ada277e85a88cf7ddc4
marcusMorck <marcus.morck@gmail.com>
2018-11-05 10:28:26 +0100

    added router function

--- index.php
+++ index.php
@@ -1,15 +1,25 @@
 <?php
- include "header.php";
-?>
-<div class="fullscreen-video-wrap">
-  <div class="video-wrap">
-    <video autoplay muted loop>
-      <source src="movies/chryslermov.mp4" type="video/mp4">
-    </video>
-  </div>  
-</div>
 
-<?php
- include "footer.php";
-?>
+$request = $_SERVER['REDIRECT_URL'];
 
+switch ($request) {
+    case '/':
+        require __DIR__ . '/views/index.php';
+        break;
+    case '':
+        require __DIR__ . '/views/index.php';
+        break;
+    case '/about-me':
+        require __DIR__ . '/views/about-me.php';
+        break;
+    case '/contact':
+        require __DIR__ . '/views/contact.php';  
+        break;
+    case '/portfolio':
+        require __DIR__ . '/views/porfolio.php';  
+        break;
+    default:
+        require __DIR__ . '404.php';
+        break;
+}
+?>
---
+++ views/index.php
@@ -0,0 +1,14 @@
+<?php
+ include "header.php";
+?>
+<div class="fullscreen-video-wrap">
+  <div class="video-wrap">
+    <video autoplay muted loop>
+      <source src="movies/chryslermov.mp4" type="video/mp4">
+    </video>
+  </div>  
+</div>
+
+<?php
+ include "footer.php";
+?>
