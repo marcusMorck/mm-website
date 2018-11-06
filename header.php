2226e564f6d077b4dcd482fc1d79fce5b8f5a09d
marcusMorck <marcus.morck@gmail.com>
2018-11-06 10:27:08 +0100

    added sass

--- header.php
+++ header.php
@@ -1,24 +1,21 @@
 <html>
     <head>
-    <link rel="stylesheet" type="text/css" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/general.css" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
     </head>
     <body>
         <header>
-            <div id="header-content">
-        <a href="index.php">
-            <div class="logo">
-                    <img src="images/profil-image.jpeg" />
-               <h1>Marcus Morck</h1>
-               
-            </div>
+        <div class="logo">
+            <a href="/">
+                <img src="images/profil-image.jpeg" />
+                <h1>Marcus Morck</h1>
             </a>
+        </div>
             <nav class="main-nav">
                 <a href="about-me.php" class="active">About me</a>
                 <a href="portfolio.php">Portfolio</a> 
                 <a href="#">Resume</a>
                 <a href="contact.php">Contact</a> 
             </nav>
-</div> 
         </header>
